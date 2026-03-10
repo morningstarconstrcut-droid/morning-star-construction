e9837d76b9e8d873d8cf56ce20a3b1118d048109
PeterAvila24 <avilapeter29@gmail.com>
2026-03-09 23:03:32 -0500

    Lower banner mobile

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
   const opacity = useTransform(scrollY, [0, 400], [1, 0.3]); // text fade
 
   return (
-    <section className="relative min-h-[calc(100vh-64px)] pt-20 flex items-center justify-center overflow-hidden md:pt-0">
+    <section className="relative min-h-[calc(100vh-64px)] pt-30 flex items-center justify-center overflow-hidden md:pt-0">
       
       {/* Background images */}
       <AnimatePresence>
